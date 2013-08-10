c919cd87032d223fabfd70dc63b587f00489712d
lorenzhart <nekogusa9@gmail.com>
2013-08-11 04:32:47 +0900

    herokuなおし、uidにlimitつけた

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130810161801) do
+ActiveRecord::Schema.define(version: 20130810193019) do
 
   create_table "answers", force: true do |t|
     t.text     "answer"

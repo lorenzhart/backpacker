a952fee1bdccb6841655a76ce10ec06988aa9801
lorenzhart <nekogusa9@gmail.com>
2013-08-11 04:18:11 +0900

    herokuのバグチェックのため再度

--- db/schema.rb
+++ db/schema.rb
@@ -45,12 +45,12 @@ ActiveRecord::Schema.define(version: 20130810161801) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                            default: "", null: false
+    t.string   "encrypted_password",               default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0
+    t.integer  "sign_in_count",                    default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 20130810161801) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
-    t.integer  "uid"
+    t.integer  "uid",                    limit: 8
     t.string   "provider"
     t.string   "token"
   end

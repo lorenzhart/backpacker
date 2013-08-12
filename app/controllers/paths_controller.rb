51594a07fcf2fc82c20804e44d61f513ac9d2683
lorenzhart <nekogusa9@gmail.com>
2013-08-12 23:03:51 +0900

    未登録ユーザー紹介時にとりあえずpathにint化したuidをつっこんどくの追加。次にuserへ勧誘のアラートを送る機能

--- app/controllers/paths_controller.rb
+++ app/controllers/paths_controller.rb
@@ -1,18 +1,14 @@
 class PathsController < ApplicationController
 
-
-
-def create
-	
-	@path = Path.new(params.require(:path).permit(:question, :to_user))
-	#@path.question = :question
-	@path.from_user = current_user.id
-
-	if @path.save
-		redirect_to question_path
-	else
-		render 'error'
+	def create	
+		@path = Path.new(params.require(:path).permit(:to_user_id))
+		@path.question = params[:quesition_id]
+		@path.from_user_id = current_user.id
+		if @path.save
+			redirect_to question_path(params[:question_id])
+		else
+			render 'error'
+		end
 	end
-end
 
 end

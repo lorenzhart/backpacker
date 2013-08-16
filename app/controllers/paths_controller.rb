c8f8aa900fd2a869e1ec32c5d5cf4e39fb852f1a
lorenzhart <nekogusa9@gmail.com>
2013-08-16 23:43:20 +0900

    jsでバグ

--- app/controllers/paths_controller.rb
+++ app/controllers/paths_controller.rb
@@ -4,14 +4,13 @@ class PathsController < ApplicationController
 		@path = Path.new(params.require(:path).permit(:to_user_id))
 		@path.question = params[:quesition_id]
 		@path.from_user_id = current_user.id
-		if @path.save
-			redirect_to question_path(params[:question_id])
-		else
+		if not @path.save
 			render 'error'
 		end
 
-		if not User.find(params[:to_user_id])  
-			redirect_to question_path(params[:question_id]), :flash => {:flag => "1"}
+
+		if not User.find_by_id(params[:to_user_id])  
+			redirect_to question_path(params[:question_id]), :flash => {:flag => "1", :dialog_request_id => params[:to_user_id]}
 		end
 	end
 

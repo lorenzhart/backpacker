aa807e83bbc04e178a5f56c831d54657453c58ad
lorenzhart <nekogusa9@gmail.com>
2013-08-18 13:32:09 +0900

    pathの表示を作る

--- app/controllers/paths_controller.rb
+++ app/controllers/paths_controller.rb
@@ -9,19 +9,8 @@ class PathsController < ApplicationController
 		end
 
 		if not User.find_by_id(params[:to_user_id])  
-			redirect_to question_path(params[:question_id]), :flash => {:flag => "1", :dialog_request_id => params[:path][:to_user_id], :test => @graph}
+			redirect_to question_path(params[:question_id]), :flash => {:flag => "1", :dialog_request_id => params[:path][:to_user_id]}
 		end
-
-#@graph = Koala::Facebook::API.new(current_user.omnitoken)
-#@graph.put_object("me", "apprequests", {:appid => MY_APP_ID, :to => USER_ID,  :message => "Come join me!"} )
-
-
-#def facebook_friends
-#  access_token = self.token
-#  @graph = Koala::Facebook::API.new(access_token)
-#  friends = @graph.get_connections("me", "friends")
-#  return friends
-#end
 
 
 	end
